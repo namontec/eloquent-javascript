30089dbbeae1b8dd40cd209e40e5d6fa85bb36d3
Victor Potasyev <vpotasyev@gmail.com>
2017-08-11 22:28:29 +0300

    finish exercise

--- eloquent4.js
+++ eloquent4.js
@@ -59,10 +59,48 @@ console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
 
 
-//=====
+//=======
 // A list
 
-console.log(arrayToList([10, 20]));
+function arrayToList(array) {
+    var list = null;
+    for (i=array.length-1; i>=0; i--) {
+        list = {value: array[i], rest: list};
+    }
+    return list;
+}
+
+function listToArray(list) {
+    var array = [];
+    for (var node = list; node; node=node.rest) {
+        array.push(node.value);
+    }
+    return array;
+}
+
+function prepend(element, list) {
+    return list = {value: element, rest: list};
+}
+
+function nth(list, number) {
+    var i=0;
+    for (var node = list; node; node=node.rest) {
+        if (i == number) {return node.value}
+        i++;
+    }
+    return undefined;
+}
+
+function nth_v2(list, number) {
+    for (var i=0; i<number; i++) {
+        if (list.rest==null) return undefined;
+        list = list.rest;
+    }
+    return list.value;
+}
+
+console.log("A list");
+console.log(arrayToList([10, 20 ]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
@@ -70,3 +108,57 @@ console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
+
+
+//===============
+//Deep comparison
+function deepEqual_v1(obj1, obj2) {
+    
+    if ((typeof obj1 == "object" && obj1 != null ) && (typeof obj2 == "object" && obj2 != null )) {
+        //если оба объекты, то продолжаем - смотрим сколько полей у каждого объекта;
+        if (countProperties(obj1) == (countProperties(obj2))) {
+            //если равны > преверяем подробнее
+            for (var prop in obj1) {
+                //если поля есть у обоих обхектов, то сравниваем
+                if (prop in obj2) return deepEqual(obj1[prop], obj2[prop])
+                    else return false;
+                }                
+            }
+        else return false;
+    }    
+    else return obj1 === obj2; //если простые переменные - просто сравниваем
+}
+
+function deepEqual(o1, o2) {
+    if ((typeof o1 == "object" && o1 != null ) && (typeof o2 == "object" && o2 != null )) {
+
+        if (countProperties(o1) != (countProperties(o2))) return false;
+        for (var prop in o1) {
+            if (prop in o2) {
+                if (!(deepEqual(o1[prop], o2[prop]))) return false;
+            } else return false;
+        }
+        return true;
+    } else return o1 === o2;
+}
+
+function countProperties(obj) {
+    var i = 0;
+    for (var prop in obj) {
+        i++
+    }
+    return i;
+}
+
+console.log("Deep comparision");
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
+
+var xx={here: {is: "an"}, object: 2};
+var yy={here: {is: "an"}, object: 2};
+console.log("Final: "+ deepEqual(xx,yy));
