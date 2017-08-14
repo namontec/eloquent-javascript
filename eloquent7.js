ce92cbd472e83a7355f7e9179cc193a8eaecff18
Victor Potasyev <vpotasyev@gmail.com>
2017-08-14 21:43:14 +0300

    finish excercise

--- eloquent7.js
+++ eloquent7.js
@@ -1,6 +1,35 @@
-function Rabbit(type) {
-  this.type = type;
+//Exercises
+
+
+//A vector type
+//=============
+
+// Your code here.
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
 }
 
-var killerRabbit = new Rabbit("killer");
-console.log(killerRabbit.type)
+Vector.prototype.plus = function(vector) {
+  return new Vector(this.x + vector.x, this.y + vector.y)
+}
+
+Vector.prototype.minus = function(vector) {
+  return new Vector(this.x - vector.x, this.y - vector.y)
+}
+
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+});
+
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
+
+
